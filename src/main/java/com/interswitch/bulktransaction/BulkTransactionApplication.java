1ebe7c2e5c7adf08eb09a23c0df736ffad52f2fc
noah.akoni <noah.akoni@ubagroup.com>
2025-11-02 23:08:30 +0100

    add bulk transaction main

--- src/main/java/com/interswitch/bulktransaction/BulkTransactionApplication.java
+++ src/main/java/com/interswitch/bulktransaction/BulkTransactionApplication.java
@@ -1,13 +1,16 @@
 package com.interswitch.bulktransaction;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class BulkTransactionApplication {
+	;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BulkTransactionApplication.class, args);
+		log.info("==>>BULK TRANSACTION PROCESSING SERVICE started");
 	}
-
 }
